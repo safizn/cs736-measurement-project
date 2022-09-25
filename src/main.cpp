d39b714bf87795289f038c3b71a122a535c60423
Safi <nassarsafi@gmail.com>
2022-09-25 12:27:50 -0500

    ➕ Buffer data generator

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <cassert>
 #include <bitset> 
+#include <stdio.h>
 
+#include "Buffer.h"
 #include "Pipe.h"
 #include "Socket.h"
 #include "SharedMemory.h"
 #include "Profiler.h"
 
-
 int main(int, char**) {
   assert(utility::checkCpp20Support() == 0); // check if compiler supports C++20 features
 
-  // transfer data - 4, 16, 64, 256, 1K, 4K, 16K, 64K, 256K, and 512K bytes
+  constexpr size_t bufferLengthInBytes = 1024;
+  std::vector<uint64_t> d = Buffer<uint64_t>::generateDataInBytes(bufferLengthInBytes);
+  printf("buffer size: %i \n", Buffer<uint64_t>::sizeof_vector(d));
+  
+  // transfer data
   unsigned long long int data{0b10101010101011111101010110000};  // in binary
   constexpr std::size_t data_size = std::numeric_limits<unsigned long long int>::digits;
   printf("Number of bytes in data = %u \n", sizeof(data));
