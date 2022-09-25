6b2c53571838dd00841da0aa8fc5c60233720069
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2022-09-25 14:49:55 -0500

    prototype ennicened rdtsc timer code

---
+++ phoenix/rdtsc_timer.c
@@ -0,0 +1,133 @@
+
+#define _GNU_SOURCE
+
+#include "rdtsc_timer.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/platform/x86.h>
+#include <sys/resource.h>
+
+
+/**
+ * Get the current CPU's current timestamp counter value using the rdtsc
+ * instruction.
+ *
+ * adapted from:
+ * https://github.com/fordsfords/rdtsc/blob/main/rdtsc.h
+ */
+static inline uint64_t rdtsc() {
+    uint32_t be, le;
+    // explanation why volatile is necessary:
+    // https://stackoverflow.com/a/26456845
+    __asm__ volatile ("rdtsc" : "=a" (le), "=d" (be));
+    return ((uint64_t) be) << 32 | ((uint64_t) le);
+}
+
+
+struct rdtsc_timer {
+    uint64_t cpu_hz;
+    /**
+     * Start timestamp, or 0 if not in started state.
+     */
+    uint64_t start;
+    /**
+     * Estimated overhead of starting and stopping the timer in nanoseconds,
+     * which is subtracted from measurements.
+     */
+    double overhead;
+};
+
+inline void start_rdtsc_timer(struct rdtsc_timer *timer) {
+    timer->start = rdtsc();
+}
+
+/**
+ * Part of `stop_timer` implementation that doesn't need to be inlined.
+ */
+static double stop_timer_ool(struct rdtsc_timer *timer, uint64_t end);
+
+inline double stop_timer(struct rdtsc_timer *timer) {
+    uint64_t end = rdtsc();
+    stop_timer_ool(timer, end);
+}
+
+static double stop_timer_ool(struct rdtsc_timer *timer, uint64_t end) {
+    // assert actually is running
+    if (!timer->start) {
+        fprintf(stderr, "stop_timer on non-running timer, aborting\n");
+        exit(1);
+    }
+
+    // return timer to non-running state
+    uint64_t start = timer->start;
+    timer->start = 0;
+
+    // do the following floating point arithmetic in a strategic order to avoid
+    // loss of significant bits assuming that the time measured is a relatively
+    // small number of nanoseconds
+    return
+        // divide "number of timestamps" by "timestamps per nanosecond"
+        // producing "number of nanoseconds".
+        //
+        // since these are, for small measurements, both within a few orders of
+        // of magnitude of 1, meaning that they're relatively close to each
+        // other in magnitude, that should minimize loss of significant digits
+        // due to floating point rounding.
+        (double) (
+            // compute number of timestamps, producing a value within a few
+            // orders of magnitude of 1 for tiny measurements
+            end_ts - start_ts
+        )
+        / (
+            // convert from timestamps per second to timestamps per nanosecond,
+            // producing a value within a few order of magnitudes of 1
+            (double) timer->cpu_hz
+            / (double) (1000 * 1000 * 1000)
+        )
+        // subtract the estimated overhead.
+        - timer->overhead_ns;
+}
+
+/**
+ * Procedurally estimate and initialize an rdtsc timer's `overhead` value.
+ */
+static void calibrate_overhead(struct rdtsc_timer_t *timer) {
+    int runs = 100000000;
+    printf("calibrating timer overhead with %i runs...\n", runs);
+
+    // we will run the calibration with overhead 0
+    timer->overhead = 0.0;
+
+    // simply compute the average nanoseconds it measures when you start the
+    // timer then stop it with no additional instructions in between.
+    double sum = 0.0;
+    for (int i = 0; i < runs; i++) {
+        start_timer(timer);
+        double elapsed = stop_timer(timer);
+        sum += elapsed;
+    }
+
+    timer->overhead_ns = sum / (double) runs;
+
+    printf(
+        "calibration complete, took %f s, timer overhead estimated at %f ns\n",
+        sum / (1000 * 1000 * 1000), timer->overhead_ns
+    );
+}
+
+void init_rdtsc_timer(struct rdtsc_timer *timer, uint64_t cpu_hz) {
+    // assert CPU has the "invariant tsc" feature
+    //
+    // otherwise the timestamps per nanosecond will go up and down as the CPU
+    // transitions between different states such as to save power by decreasing
+    // clock speed, so this timer won't work.
+    if (!CPU_FEATURE_PRESENT(INVARIANT_TSC)) {
+        fprintf(stderr, "CPU does not support invariant TSC, aborting\n");
+        exit(1);
+    }
+
+    timer->cpu_hz = cpu_hz;
+    timer->start = 0;
+
+    calibrate_overhead(timer);
+}
---
+++ phoenix/rdtsc_timer.h
@@ -0,0 +1,39 @@
+/**
+ * A precise timer we use for our experiments based on the rdtsc instruction.
+ *
+ * This breaks if the thread moves between CPUs, so the user must pin the
+ * thread to a single CPU before using to ensure correctness.
+ */
+
+
+#ifndef RDTSC_TIMER
+#define RDTSC_TIMER
+
+#include <stdint.h>
+
+
+/**
+ * Precise timer for our experiments, based on the rdtsc instruction.
+ */
+struct rdtsc_timer;
+
+
+/**
+ * Initialize and calibrate an rdtsc timer.
+ * 
+ * The user should be able to find the cpu hz with lscpu.
+ */
+void init_rdtsc_timer(struct rdtsc_timer *timer, uint64_t cpu_hz);
+
+/**
+ * Start an initialized rdtsc timer.
+ */
+void start_rdtsc_timer(struct rdtsc_timer *timer);
+
+/**
+ * Stop a running rdtsc timer, and return the elapsed nanoseconds.
+ */
+double stop_timer(struct rdtsc_timer *timer);
+
+
+#endif RDTSC_TIMER
