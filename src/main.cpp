1994e6a0004e83c3205869a11754cb883f53d16e
Safi <nassarsafi@gmail.com>
2022-09-25 12:45:02 -0500

    ➕ Implement data generator class.

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,14 @@ int main(int, char**) {
   assert(utility::checkCpp20Support() == 0); // check if compiler supports C++20 features
 
   constexpr size_t bufferLengthInBytes = 1024;
-  std::vector<uint64_t> d = Buffer<uint64_t>::generateDataInBytes(bufferLengthInBytes);
-  printf("buffer size: %i \n", Buffer<uint64_t>::sizeof_vector(d));
-  
+  Buffer b {bufferLengthInBytes}; 
+  b.printBinaryData();
+  b.printSizeInfo(); 
+
   // transfer data
   unsigned long long int data{0b10101010101011111101010110000};  // in binary
   constexpr std::size_t data_size = std::numeric_limits<unsigned long long int>::digits;
-  printf("Number of bytes in data = %u \n", sizeof(data));
+  cout << data_size << endl;
 
   // instances
   Pipe<decltype(data)> i1{data, "pipe instance A"};
