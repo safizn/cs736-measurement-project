ef9ace21cbca985da15aece026a1fa52688da74b
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2022-09-27 10:30:20 -0500

    fix spacing

--- phoenix/ipc_mechanism_pipe.c
+++ phoenix/ipc_mechanism_pipe.c
@@ -10,17 +10,17 @@ int main(int argc, char* argv[]) {
 	char child_buf[1000000];
 		
 	//create pipe
-	if(pipe(pipefds) == -1){
+	if (pipe(pipefds) == -1){
 		perror("pipe");
 		exit(EXIT_FAILURE);
 	}
 	memset(par_buf,0,1000000);
 	pid = fork();
 	if (pid > 0) {
-		printf(" PARENT write in pipe\n");
+		printf("PARENT write in pipe\n");
 		//parent close the read end
 		close(pipefds[0]);
-		for(int i=0;i<1000000;i++)
+		for (int i=0; i<1000000; i++)
 			par_buf[i]='a';
 		//parent write in the pipe write end                 
 		write(pipefds[1], par_buf, 1000000);
@@ -30,9 +30,9 @@ int main(int argc, char* argv[]) {
 		wait(NULL);                         
 	} else {
 		//child close the write end  
-		close(pipefds[1]);   //-----line *
+		close(pipefds[1]);
 		//child read from the pipe read end until the pipe is empty   
-		while(read(pipefds[0], child_buf, 1)==1)   
+		while (read(pipefds[0], child_buf, 1) == 1)   
 			printf("CHILD read from pipe -- %s\n", child_buf);
 		//after finishing reading, child close the read end
 		close(pipefds[0]);
