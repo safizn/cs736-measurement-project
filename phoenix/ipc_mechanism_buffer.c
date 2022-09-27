4edeb0f4f9e55042f4cd179f4a9663d166c72824
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2022-09-27 12:03:22 -0500

    unchecked draft ipc_mechanism_buffer.c

--- phoenix/ipc_mechanism_buffer.c
+++ phoenix/ipc_mechanism_buffer.c
@@ -1,4 +1,5 @@
 
+#include "ipc_mechanism.h"
 #include <math.h>
 #include <x86intrin.h>
 #include <stdio.h>
@@ -15,88 +16,91 @@
 
 #define AVGITER 300
 
-int SIZE = 0;
+int SIZE = 1000000;
 unsigned long long a, b;
 struct timeval t1, t2;
 double elapsedTime;
 
-void piping()
-{
-    int pipefd[2], pipefd1[2];
-    pid_t pid;
-    char parent_write_buffer[SIZE];
-    char child_buffer[SIZE];
-    char parent_read_buffer[SIZE];
+//void piping()
+//{
+//    int pipefd[2], pipefd1[2];
+//    pid_t pid;
+//    char parent_write_buffer[SIZE];
+//    char child_buffer[SIZE];
+//    char parent_read_buffer[SIZE];
+//
+//    sem_t *semptr = sem_open("/semaphore", O_CREAT | O_EXCL, 0644, 0); /* Note O_EXCL */
+//    sem_unlink("/semaphore");
+//    if ((pipe(pipefd) == -1) || (pipe(pipefd1) == -1))
+//    {
+//        perror("Error while creating Pipes\nExiting\n");
+//        exit(-1);
+//    }
+//    if ((pid = fork()) == -1)
+//    {
+//        perror("Error while creating Child Process\nExiting\n");
+//        exit(-1);
+//    }
+//    else if (pid == 0)
+//    {
+//        // The TChild Process
+//        // Closing Write Buffer of Pipe 1
+//        close(pipefd[1]);
+//        close(pipefd1[0]); // Closing Read Buffer of the Pipe
+//        sem_wait(semptr);
+//
+//        // Reading pointer to the file stored in the Buffer and assign it to fp
+//        read(pipefd[0], child_buffer, sizeof(child_buffer));
+//
+//        // Now writing Contents of File to Write Buffer of second pipe
+//
+//        write(pipefd1[1], child_buffer, sizeof(child_buffer));
+//
+//        // Closing Write Buffer of Second Pipe
+//        close(pipefd1[1]);
+//
+//        exit(0);
+//    }
+//    else
+//    {
+//        // This is the Parent Process
+//        close(pipefd[0]);
+//        // Closing Write Buffer of the Second Pipe
+//        close(pipefd1[1]);
+//        for (int i = 0; i < SIZE; i++)
+//        {
+//            parent_write_buffer[i] = 'a';
+//        }
+//
+//        gettimeofday(&t1, NULL);
+//        a = __rdtsc();
+//
+//        write(pipefd[1], parent_write_buffer, sizeof(parent_write_buffer));
+//        // Close Writer Buffer of the Pipe
+//        close(pipefd[1]);
+//        sem_post(semptr);
+//        // Wait for Child Process to complete its execution
+//        wait(NULL);
+//        // Reading Contents stored in the Buffer
+//        read(pipefd1[0], parent_read_buffer, sizeof(parent_read_buffer));
+//        b = __rdtsc();
+//        gettimeofday(&t2, NULL);
+//
+//        close(pipefd1[0]); // Closing Read Buffer of the Second Pipe
+//        // fprintf(stdout, "Contents of the File are\n%s\n", parent_read_buffer);
+//    }
+//}
 
-    sem_t *semptr = sem_open("/semaphore", O_CREAT | O_EXCL, 0644, 0); /* Note O_EXCL */
-    sem_unlink("/semaphore");
-    if ((pipe(pipefd) == -1) || (pipe(pipefd1) == -1))
-    {
-        perror("Error while creating Pipes\nExiting\n");
-        exit(-1);
-    }
-    if ((pid = fork()) == -1)
-    {
-        perror("Error while creating Child Process\nExiting\n");
-        exit(-1);
-    }
-    else if (pid == 0)
-    {
-        // The TChild Process
-        // Closing Write Buffer of Pipe 1
-        close(pipefd[1]);
-        close(pipefd1[0]); // Closing Read Buffer of the Pipe
-        sem_wait(semptr);
 
-        // Reading pointer to the file stored in the Buffer and assign it to fp
-        read(pipefd[0], child_buffer, sizeof(child_buffer));
+struct ipc_throughput_test {
+    void *memptr;
+    char *parent_write_buffer;
+    sem_t *semptr;
+};
 
-        // Now writing Contents of File to Write Buffer of second pipe
-
-        write(pipefd1[1], child_buffer, sizeof(child_buffer));
-
-        // Closing Write Buffer of Second Pipe
-        close(pipefd1[1]);
-
-        exit(0);
-    }
-    else
-    {
-        // This is the Parent Process
-        close(pipefd[0]);
-        // Closing Write Buffer of the Second Pipe
-        close(pipefd1[1]);
-        for (int i = 0; i < SIZE; i++)
-        {
-            parent_write_buffer[i] = 'a';
-        }
-
-        gettimeofday(&t1, NULL);
-        a = __rdtsc();
-
-        write(pipefd[1], parent_write_buffer, sizeof(parent_write_buffer));
-        // Close Writer Buffer of the Pipe
-        close(pipefd[1]);
-        sem_post(semptr);
-        // Wait for Child Process to complete its execution
-        wait(NULL);
-        // Reading Contents stored in the Buffer
-        read(pipefd1[0], parent_read_buffer, sizeof(parent_read_buffer));
-        b = __rdtsc();
-        gettimeofday(&t2, NULL);
-
-        close(pipefd1[0]); // Closing Read Buffer of the Second Pipe
-        // fprintf(stdout, "Contents of the File are\n%s\n", parent_read_buffer);
-    }
-}
-
-
-
-void shming() {
-
-    char parent_write_buffer[SIZE];
-    for (int i = 0; i < SIZE; i++)
-        parent_write_buffer[i] = 'a';
+void init_ipc_throughput_test(struct ipc_throughput_test **test) {
+    //char parent_write_buffer[SIZE];
+    
     /* get a pointer to memory */
     void *memptr = mmap(NULL,                       /* let system pick where to put segment */
                         SIZE,                       /* how many bytes */
@@ -132,57 +136,76 @@ void shming() {
     }
     else
     {
+        char *parent_write_buffer = malloc(SIZE);
+        for (int i = 0; i < SIZE; i++)
+            parent_write_buffer[i] = 'a';
+
+        *test = malloc(sizeof(struct ipc_throughput_test));
+        (*test)->memptr = memptr;
+        (*test)->parent_write_buffer = parent_write_buffer;
+        (*test)->semptr = semptr;
+
+        return SIZE;
         // Parent Process
         // printf("Writing in memory: Parent\n");
 
-        gettimeofday(&t1, NULL);
-        a = __rdtsc();
+        //gettimeofday(&t1, NULL);
+        //a = __rdtsc();
 
-        memcpy(memptr, parent_write_buffer, SIZE);
-        sem_post(semptr);
+        
 
-        b = __rdtsc();
-        gettimeofday(&t2, NULL);
+        //b = __rdtsc();
+        //gettimeofday(&t2, NULL);
 
         /* cleanup */
-        wait(NULL);
-        munmap(memptr, SIZE);
-        sem_close(semptr);
     }
 }
 
-int main()
-{
-
-    int sizes[] = {4, 16, 64, 256, 512, 1024, 2048, 4096, 16 * 1024, 64 * 1024, 128 * 1024, 256 * 1024};
-
-    unsigned long cpuMask;
-    cpuMask = 2; // bind to cpu 1
-    if (!sched_setaffinity(0, sizeof(cpuMask), &cpuMask))
-        fprintf(stderr, "Running on one core!\n");
-    setpriority(PRIO_PROCESS, 0, 20);
-
-    for (int i = 0; i < 12; i++)
-    {
-        double averageTimeOfDay=0;
-        double averageTimeUsingRdtsc=0;
-        for(int j=0;j<1000;j++){
-            SIZE = sizes[i];
-
-            piping();
-
-            // printf("a:%llu\n", a);
-            // printf("b:%llu\n", b);
-            double val = ((b - a) / 2700);
-            averageTimeUsingRdtsc+=val;
-            printf("Size: %d - Time through rdtsc in microsec:%f\n", SIZE, val);
-            // fprintf(stderr, "Time 1st through rdtsc in microsec:%f\n\nSubtraction:%llu\n\n", val, b - a);
-            elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000.0; // sec to ms
-            elapsedTime += (t2.tv_usec - t1.tv_usec);   
-            averageTimeOfDay+=elapsedTime;       // / 1000.0; // us to ms
-            printf("Size: %d - Time through gettimeofday in us:%f\n\n", SIZE, elapsedTime);
-        }
-        printf("Size: %d - average tod:%f", SIZE, averageTimeOfDay/1000);
-        printf("Size: %d - averagetimertdsc:%f", SIZE, averageTimeUsingRdtsc/1000);
-    }
+void run_ipc_throughput_test(struct ipc_throughput_test *test) {
+    memcpy(test->memptr, test->parent_write_buffer, SIZE);
+    sem_post(test->semptr);
 }
+
+void free_ipc_throughput_test(struct ipc_throughput_test *test) {
+    wait(NULL);
+    munmap(test->memptr, SIZE);
+    sem_close(test->semptr);
+    free(test->parent_write_buffer);
+    free(test);
+}
+
+//int main()
+//{
+//
+//    int sizes[] = {4, 16, 64, 256, 512, 1024, 2048, 4096, 16 * 1024, 64 * 1024, 128 * 1024, 256 * 1024};
+//
+//    unsigned long cpuMask;
+//    cpuMask = 2; // bind to cpu 1
+//    if (!sched_setaffinity(0, sizeof(cpuMask), &cpuMask))
+//        fprintf(stderr, "Running on one core!\n");
+//    setpriority(PRIO_PROCESS, 0, 20);
+//
+//    for (int i = 0; i < 12; i++)
+//    {
+//        double averageTimeOfDay=0;
+//        double averageTimeUsingRdtsc=0;
+//        for(int j=0;j<1000;j++){
+//            SIZE = sizes[i];
+//
+//            piping();
+//
+//            // printf("a:%llu\n", a);
+//            // printf("b:%llu\n", b);
+//            double val = ((b - a) / 2700);
+//            averageTimeUsingRdtsc+=val;
+//            printf("Size: %d - Time through rdtsc in microsec:%f\n", SIZE, val);
+//            // fprintf(stderr, "Time 1st through rdtsc in microsec:%f\n\nSubtraction:%llu\n\n", val, b - a);
+//            elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000.0; // sec to ms
+//            elapsedTime += (t2.tv_usec - t1.tv_usec);   
+//            averageTimeOfDay+=elapsedTime;       // / 1000.0; // us to ms
+//            printf("Size: %d - Time through gettimeofday in us:%f\n\n", SIZE, elapsedTime);
+//        }
+//        printf("Size: %d - average tod:%f", SIZE, averageTimeOfDay/1000);
+//        printf("Size: %d - averagetimertdsc:%f", SIZE, averageTimeUsingRdtsc/1000);
+//    }
+//}//
